909d01373371b608f60f975db428777b2e75733c
Mohd Shahid <72098918+shahid1223@users.noreply.github.com>
2022-01-05 01:40:48 -0800

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-const MONGOURI = "mongodb+srv://shahid:shahid@cluster0.zbodk.mongodb.net/gadistand?retryWrites=true&w=majority"
+const MONGOURI = ""
 const connectTOMongo = () => {
   mongoose.connect(MONGOURI,()=>{
       console.log("Connected to db")
